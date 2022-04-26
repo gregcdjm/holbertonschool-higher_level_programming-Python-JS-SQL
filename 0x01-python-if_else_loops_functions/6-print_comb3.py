ebcc0c65bc0ff93a6c745bfc1e0b2ddc75d1a883
gregcdjm <gregoirecoudrin@gmail.com>
2022-04-26 15:03:11 +0200

    8

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,12 @@
-n
+#!/usr/bin/python3
+
+a = 0, b = 1
+
+while a < 8:
+    print("{}{}".format(a, b), end=", ")
+    if b == 9:
+        a += 1
+        b = a + 1
+    else:
+        b += 1
+print(89)
