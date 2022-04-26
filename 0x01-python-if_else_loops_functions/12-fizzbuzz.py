4cb41ad1e4e3c74c92067af7836b1e084337e540
gregcdjm <gregoirecoudrin@gmail.com>
2022-04-26 16:05:01 +0200

    FIIIZZZZ

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,12 @@
-while (A < 99)
-{
-A = A + 1
-if (A % (15) == 0)
-printf("FizzBuzz ")
-else if ((A / 10 + A % 10) % 3 == 0)
-printf("Fizz ")
-elif ((A % 10 == 5) or (A % 10 == 0))
-printf("Buzz ")
-else
-printf("%d ", A)
-}
-printf("Buzz\n")
-return (0);
-}
+#!/usr/bin/python3
+
+for A in range (0, 99):
+    if (A % 15) == 0:
+        print("FizzBuzz ", end="")
+    elif (A / 10 + A % 10) % 3 == 0:
+        print("Fizz ", end="")
+    elif A % 10 == 5 or A % 10 == 0:
+        print("Buzz ", end ="")
+    else:
+        print(f"{A} ", end ="")
+print("Buzz")
