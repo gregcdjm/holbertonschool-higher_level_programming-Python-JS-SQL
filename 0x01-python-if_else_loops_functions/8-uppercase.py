7ca7f5560b68bdd21f92179efc3c43f96df177c1
gregcdjm <gregoirecoudrin@gmail.com>
2022-04-26 17:03:24 +0200

    e

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,8 @@
-n
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        letter = ord(letter)
+        if letter in range(97, 123):
+            letter -= 32
+        print("{:c}".format(letter), end="")
+    print("")
