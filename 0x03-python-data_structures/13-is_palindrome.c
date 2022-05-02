8eebd63458f3f0f277f0a311ebd684ca33773e74
gregcdjm <gregoirecoudrin@gmail.com>
2022-05-02 16:31:21 +0200

    pala

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -13,26 +13,29 @@ int is_palindrome(listint_t **head)
 	listint_t *tmpD1 = *tmp, *tmpD2 = *tmp2;
 	listint_t *headD = *head;
 
-	while (tmpD1->next->next)
+	if (head != NULL || head->next != NULL))
 	{
-		tmpD1 = tmpD1->next;
-		i++;
-	}
-	k = i / 2;
-	if (i % 2 != 0)
-		j--;
-	for (; j < k + 1; j++)
-	{
-		tmpD1 = headD;
-		for (i = k * 2; i - j != 0; i--)
+		while (tmpD1->next->next)
 		{
 			tmpD1 = tmpD1->next;
+			i++;
 		}
-		if (tmpD1->next->n != tmpD2->n)
+		k = i / 2;
+		if (i % 2 != 0)
+			j--;
+		for (; j < k + 1; j++)
 		{
-			return (0);
+			tmpD1 = headD;
+			for (i = k * 2; i - j != 0; i--)
+			{
+				tmpD1 = tmpD1->next;
+			}
+			if (tmpD1->next->n != tmpD2->n )
+			{
+				return (0);
+			}
+			tmpD2 = tmpD2->next;
 		}
-		tmpD2 = tmpD2->next;
 	}
 	return (1);
 }
