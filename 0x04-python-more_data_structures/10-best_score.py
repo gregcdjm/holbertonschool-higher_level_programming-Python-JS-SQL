b98dd3159190176c22f596a0c61242d0e904977d
gregcdjm <gregoirecoudrin@gmail.com>
2022-05-06 16:14:19 +0200

    task9and10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,6 @@
- 
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        max_k = max(a_dictionary, key=a_dictionary.get)
+        return max_k
+    return None
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,6 @@
- 
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_a_dictionary = a_dictionary.copy()
+    for value in a_dictionary:
+        new_a_dictionary[value] = a_dictionary[value] * 2
+    return new_a_dictionary
