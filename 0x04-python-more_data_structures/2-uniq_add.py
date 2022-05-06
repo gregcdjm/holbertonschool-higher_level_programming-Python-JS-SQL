458081c2751400be6033f5b7be63489176c30aad
gregcdjm <gregoirecoudrin@gmail.com>
2022-05-06 16:04:30 +0200

    task1

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,9 @@
- 
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique = []
+    new_list = 0
+    for i in my_list:
+        if i not in unique:
+            unique.append(i)
+            new_list += i
+    return new_list
