4172f7ce721835056827c8a8cab6c2bcc8d3fa94
gregcdjm <gregoirecoudrin@gmail.com>
2022-05-16 23:37:30 +0200

    3first

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,11 @@
- 
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    num_print = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            num_print += 1
+        except IndexError:
+            break
+    print()
+    return num_print
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,11 @@
- 
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    num_print = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            num_print += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return num_print
