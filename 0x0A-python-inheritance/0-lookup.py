7e67acdaf9d1e066a8a0b06818a686f9075003fa
gregcdjm <gregoirecoudrin@gmail.com>
2022-06-01 15:27:30 +0200

    0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+The lookup function
+"""
+
+
+def lookup(obj):
+    """Function that returns the list of available attributes
+    and methods of an object
+    """
+    return(dir(obj))
