017a9414712ec1099dc95777a8c2597de676c00f
gregcdjm <gregoirecoudrin@gmail.com>
2022-06-01 15:55:44 +0200

    6th

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""the BaseGeometry class"""
+
+
+class BaseGeometry:
+    """the BaseGeometry class"""
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        """function that raises an Exception"""
+        raise Exception("area() is not implemented")
