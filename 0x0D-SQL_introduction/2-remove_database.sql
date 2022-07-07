015bfe938a51bb5cb12b5803737095bc3b2ac028
gregcdjm <gregoirecoudrin@gmail.com>
2022-07-07 08:23:29 +0200

    dele

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,4 +1,4 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
 SELECT id, name FROM students WHERE batch_id = 3 ORDER BY created_at DESC LIMIT 3;
-DELETE IF EXIST hbtn_0c_0;
+DROP DATABASE IF EXISTS database_name hbtn_0c_0;
