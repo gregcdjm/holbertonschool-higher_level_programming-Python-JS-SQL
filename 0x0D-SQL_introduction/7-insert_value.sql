ec25eb264d5bb5efb12729a5ca9ef5721ec08aa7
gregcdjm <gregoirecoudrin@gmail.com>
2022-07-07 14:17:35 +0200

    7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,5 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
 
-INSERT INTO first_table (id, name) VALUES (89, Best School);
+INSERT INTO first_table (id, name)
+VALUES (89, 'Best School');
