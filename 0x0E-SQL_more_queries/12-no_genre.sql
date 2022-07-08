0602c836ab692c63914b8630ee823c0514eff149
gregcdjm <gregoirecoudrin@gmail.com>
2022-07-08 23:47:02 +0200

    e

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
---- lkji
+-- script that lists all shows contained in hbtn_0d_tvshows
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
