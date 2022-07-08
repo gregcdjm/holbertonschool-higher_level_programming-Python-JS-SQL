00fa57505372bbaa8f84840ce68262beac7189ce
gregcdjm <gregoirecoudrin@gmail.com>
2022-07-08 19:02:41 +0200

    e

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 --bla bla
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
-CREATE TABLE IF NOT EXISTS cities (
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities FROM hbtn_0d_usa(
        id INT,
        state_id VARCHAR(256) NOT NULL,
        score INT,
