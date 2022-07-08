9d21004943fd8f84e5f74ff830aeef317c69ad8a
gregcdjm <gregoirecoudrin@gmail.com>
2022-07-08 19:19:49 +0200

    f

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,9 +3,9 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
        id INT NOT NULL AUTO_INCREMENT,
-       state_id INT NOT NULL,
+	   state_id INT NOT NULL,
        name VARCHAR(256) NOT NULL,
-       UNIQUE (id),
-       PRIMARY KEY (id),
-       FOREIGN KEY (state_id) REFERENCES states(id)
+	   UNIQUE (id),
+	   PRIMARY KEY (id),
+	   FOREIGN KEY (state_id) REFERENCES states(id)
 );
