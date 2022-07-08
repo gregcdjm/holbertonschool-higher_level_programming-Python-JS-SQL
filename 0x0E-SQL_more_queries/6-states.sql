f8a00c0a11e0996af08c69e726720af0868df8ab
gregcdjm <gregoirecoudrin@gmail.com>
2022-07-08 11:21:04 +0200

    e

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+       id INT NOT NULL AUTO_INCREMENT,
+       name VARCHAR(256) NOT NULL,
+       UNIQUE (id),
+       PRIMARY KEY (id)
+);
