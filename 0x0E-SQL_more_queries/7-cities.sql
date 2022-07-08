e0b1ddc915e58660dd095c8021fcde57383ccb43
gregcdjm <gregoirecoudrin@gmail.com>
2022-07-08 19:21:02 +0200

    f

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,11 +1,9 @@
---bla bla
+-- script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS states (
        id INT NOT NULL AUTO_INCREMENT,
-	   state_id INT NOT NULL,
-       name VARCHAR(256) NOT NULL,
+       name VARCHAR(256),
 	   UNIQUE (id),
-	   PRIMARY KEY (id),
-	   FOREIGN KEY (state_id) REFERENCES states(id)
+	   PRIMARY KEY (id)
 );
