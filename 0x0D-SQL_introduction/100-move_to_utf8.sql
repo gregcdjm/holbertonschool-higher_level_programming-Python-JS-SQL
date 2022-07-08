42a01c6cb682f8d359a50b2c275e7f32c6c6858a
gregcdjm <gregoirecoudrin@gmail.com>
2022-07-08 18:30:57 +0200

    f

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,2 @@
 -- file convert to utf8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE hbtn_0c_0
-SELECT name CONVERT name USING utf8
-FROM first_tabme
-WHERE CONVERT name USING utf8 IS NULL;
