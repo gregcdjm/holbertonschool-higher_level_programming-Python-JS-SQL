b1ea51698a312bab726609ce59095354d25e8c1d
gregcdjm <gregoirecoudrin@gmail.com>
2022-08-10 10:36:39 +0200

    secBig

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const entry = process.argv;
+let check;
+let big = 0;
+let secondbig = 0;
+
+for (let i = 2; i < entry.length; i++) {
+    check = parseInt(entry[i]);
+    if (isNaN(check)) {
+	continue;
+    } else {
+	if (check >= big) {
+	    secondbig = big;
+	    big = check;
+	}
+    }
+}
+console.log(secondbig);
